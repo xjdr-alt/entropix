8f49b325e7b7ed4bb813cfb7092b0207b85de5cc
Arrabonae <wexylum@protonmail.com>
2024-10-06 10:10:37 +0800

    to align with jax implementation; mcts.py added

---
+++ main.py
@@ -0,0 +1,105 @@
+from typing import NamedTuple, Optional, Tuple
+
+import torch
+import torch.nn.functional as F
+
+import math
+import tyro
+
+from pathlib import Path
+from functools import partial
+
+from entropix.config import LLAMA_1B_PARAMS
+from entropix.tokenizer import Tokenizer
+from entropix.torch_kvcache import KVCache
+from entropix.torch_model import xfmr
+from entropix.torch_weights import XfmrWeights, LayerWeights, load_weights
+from entropix.torch_sampler import sample
+from entropix.prompts import prompt, bp1
+
+
+def apply_scaling(freqs: torch.Tensor):
+    # Values obtained from grid search
+    scale_factor = 8
+    low_freq_factor = 1
+    high_freq_factor = 4
+    old_context_len = 8192  # original llama3 length
+
+    low_freq_wavelen = old_context_len / low_freq_factor
+    high_freq_wavelen = old_context_len / high_freq_factor
+    new_freqs = []
+    for freq in freqs:
+        wavelen = 2 * math.pi / freq
+        if wavelen < high_freq_wavelen:
+            new_freqs.append(freq)
+        elif wavelen > low_freq_wavelen:
+            new_freqs.append(freq / scale_factor)
+        else:
+            assert low_freq_wavelen != high_freq_wavelen
+            smooth = (old_context_len / wavelen - low_freq_factor) / (
+                high_freq_factor - low_freq_factor
+            )
+            new_freqs.append((1 - smooth) * freq / scale_factor + smooth * freq)
+    return torch.tensor(new_freqs, dtype=freqs.dtype, device=freqs.device)
+
+
+def precompute_freqs_cis(
+    dim: int, end: int, theta: float = 10000.0, use_scaled: bool = False
+):
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device, dtype=torch.float32)
+    if use_scaled:
+        freqs = apply_scaling(freqs)
+    freqs = torch.outer(t, freqs)
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
+    return freqs_cis
+
+
+def build_attn_mask(seqlen: int, start_pos: int) -> torch.Tensor:
+  mask = None
+  if seqlen > 1:
+      mask = torch.full((seqlen, seqlen), float("-inf"))
+      mask = torch.triu(mask, diagonal=1)
+      mask = torch.hstack([torch.zeros((seqlen, start_pos)), mask]).to(torch.bfloat16)
+  return mask
+
+
+def main():
+  with torch.inference_mode():
+    model_params = LLAMA_1B_PARAMS
+    xfmr_weights = load_weights()
+
+    tokenizer = Tokenizer('entropix/tokenizer.model')
+    raw_tokens1 = tokenizer.encode(prompt,  bos=False, eos=False, allowed_special='all')
+    #this is not used in this script, but can be used to generate base_raw_tokens1
+    base_raw_tokens1 = tokenizer.encode(bp1, bos=True, eos=False, allowed_special='all')
+
+
+    def generate(xfmr_weights, model_params, tokens):
+      gen_tokens = None
+      cur_pos = 0
+      tokens = torch.tensor([tokens], dtype=torch.long)
+      bsz, seqlen = tokens.shape
+      attn_mask = build_attn_mask(seqlen, cur_pos)
+      freqs_cis = precompute_freqs_cis(model_params.head_dim, model_params.max_seq_len, model_params.rope_theta, model_params.use_scaled_rope)
+      kvcache = KVCache.new(model_params.n_layers, bsz, model_params.max_seq_len, model_params.n_local_kv_heads, model_params.head_dim)
+      logits, kvcache, _, _ = xfmr(xfmr_weights, model_params, tokens, cur_pos, freqs_cis[:seqlen], kvcache, attn_mask=attn_mask)
+      next_token = torch.argmax(logits[:, -1], dim=-1, keepdim=True).to(torch.long)
+      gen_tokens = next_token
+      print(tokenizer.decode([next_token.item()]), end='', flush=True)
+      cur_pos = seqlen
+      stop = torch.tensor([128001, 128008, 128009])
+      while cur_pos < 8192:
+        cur_pos += 1
+        logits, kvcache, scores, stats = xfmr(xfmr_weights, model_params, next_token, cur_pos, freqs_cis[cur_pos:cur_pos+1], kvcache)
+        next_token = sample(gen_tokens, logits, scores)
+        gen_tokens = torch.cat((gen_tokens, next_token), dim=1)
+        print(tokenizer.decode(next_token.tolist()[0]), end='', flush=True)
+        if torch.isin(next_token, stop).any():
+          break
+
+    print(prompt)
+    generate(xfmr_weights, model_params, raw_tokens1)
+
+if __name__ == '__main__':
+  tyro.cli(main)
