80aa9262680a0c359318381db6427a6d2ec5525c
hallucinomeny <loglogplot@proton.me>
2024-10-07 02:11:48 +0300

    fixed params

--- entropix/config.py
+++ entropix/config.py
@@ -24,3 +24,26 @@ class SamplerParams(NamedTuple):
     base_temp: float
     base_top_p: float
     base_top_k: int
+
+params = {
+  "dim": 2048,
+  "n_layers": 16,
+  "n_heads": 32,
+  "n_kv_heads": 8,
+  "vocab_size": 128256,
+  "ffn_dim_multiplier": 1.5,
+  "multiple_of": 256,
+  "norm_eps": 1e-05,
+  "rope_theta": 500000.0,
+  "use_scaled_rope": True,
+  "max_seq_len": 4096
+}
+LLAMA_1B_PARAMS = ModelParams(
+  n_layers=params["n_layers"],
+  n_local_heads=params["n_heads"],
+  n_local_kv_heads=params["n_kv_heads"],
+  head_dim=params["dim"] // params["n_heads"],
+  max_seq_len=params["max_seq_len"],
+  rope_theta=params["rope_theta"],
+  use_scaled_rope=params["use_scaled_rope"]
+)
