2f9012b2f1babb49d5e7d38fd6eb8121e384eaf8
ecal <ethan@ecal.dev>
2024-10-08 19:24:24 -0400

    Chat request pydantic model

--- entropix/server/server.py
+++ entropix/server/server.py
@@ -37,8 +37,6 @@ class ServerArgs(BaseModel):
     assert self.model_path.exists(), f"Model path ({self.model_path}) does not exist."
     if self.tokenizer_path is None: self.tokenizer_path = self.model_path
 
-
-
 class Message(BaseModel):
   class ToolCallFunction(BaseModel):
     name: str
@@ -56,19 +54,60 @@ class Message(BaseModel):
   tool_call_id: str | None = None
 
   @model_validator(mode='after')
-  def role_tool_if_tool_calls(self) -> Self:
+  def validate_role_restricted_params(self) -> Self:
     if self.role != "assistant" and self.tool_calls is not None:
       raise ValueError("Only assistant messages can have tool_calls")
     elif self.role == "tool" and self.tool_call_id is None:
       raise ValueError("Tool messages must have a tool_call_id")
-
     return self
 
 
 class ChatRequest(BaseModel):
+  # https://platform.openai.com/docs/api-reference/chat
+  # omitted some openai specific stuff (store, metadata, etc)
+
   messages: list[Message]
+  model: str | None = None  # currently just preloading and serving a model through server args
+  frequency_penalty: float = 0.0
+  logit_bias: dict[int, float] | None = None
+  logprobs: bool = False
+  top_logprobs: int | None = None
+  max_completion_tokens: int | None = None
+  n: int = 1
+  presence_penalty: float = 0.0
+  response_format: dict | None = None
+  seed: int | None = None
+  stop: str | list[str] | None = None
   stream: bool = False
-  temperature: float = 0.7
+  stream_options: dict | None = None
+  temperature: float = 1.0
+  top_p: float = 1.0
+  tools: list[dict] | None = None
+  tool_choice: str | dict | None = None
+
+  # NOTE: may want to change limits, just using OpenAI values for now
+
+  @field_validator('frequency_penalty', 'presence_penalty')
+  def validate_penalty(cls, v):
+    if not -2.0 <= v <= 2.0: raise ValueError("penalties must be between -2.0 and 2.0")
+    return v
+
+  @field_validator('temperature', 'top_p')
+  def validate_sampling_params(cls, v):
+    if not 0 <= v <= 2: raise ValueError("temperature and top_p must be between 0 and 2")
+    return v
+
+  @field_validator('n')
+  def validate_n(cls, v):
+    if v < 1: raise ValueError("n must be >=1")
+    return v
+
+  @field_validator('top_logprobs')
+  def validate_top_logprobs(cls, v):
+    if v is not None and not 0 <= v <= 20:  # TODO: make this full vocab length (or just don't validate?)
+      raise ValueError("top_logprobs must be between 0 and 20")
+    return v
+
 
 weights_path = Path('weights/1B-Instruct')
 model_params = LLAMA_1B_PARAMS
@@ -96,7 +135,7 @@ async def health() -> Response:
 
 @app.post("/v1/chat/completions")
 async def openai_chat_completions(request: ChatRequest):
-  logger.info(request.prompt)
+  logger.info(request)
 
   tokens = tokenizer.encode(request.prompt, bos=False, eos=False, allowed_special='all')
 
