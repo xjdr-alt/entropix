8941adac93f191c4f5f1621e39675af4a47c43dd
ecal <ethan@ecal.dev>
2024-10-08 17:48:41 -0400

    Message request types

--- entropix/server/server.py
+++ entropix/server/server.py
@@ -2,6 +2,8 @@ import logging
 import threading
 from contextlib import asynccontextmanager
 from pathlib import Path
+from typing import Literal
+from typing_extensions import Self
 
 import jax
 import jax.numpy as jnp
@@ -10,7 +12,7 @@ from fastapi import FastAPI, File, Form, Request, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, Response, StreamingResponse
 from jax._src.typing import Array
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, ValidationError, field_validator, model_validator
 import uvicorn
 import asyncio
 import uvloop
@@ -35,9 +37,38 @@ class ServerArgs(BaseModel):
     assert self.model_path.exists(), f"Model path ({self.model_path}) does not exist."
     if self.tokenizer_path is None: self.tokenizer_path = self.model_path
 
-class PromptRequest(BaseModel):
-  prompt: str
+
+
+class Message(BaseModel):
+  class ToolCallFunction(BaseModel):
+    name: str
+    arguments: str
+
+  class ToolCall(BaseModel):
+    id: str
+    type: str  # only "function" is currently supported in openai api
+    function: "Message.ToolCallFunction"
+
+  content: str | list[str]
+  role: Literal["system", "user", "assistant", "tool"]
+  name: str | None = None
+  tool_calls: list[ToolCall] | None = None
+  tool_call_id: str | None = None
+
+  @model_validator(mode='after')
+  def role_tool_if_tool_calls(self) -> Self:
+    if self.role != "assistant" and self.tool_calls is not None:
+      raise ValueError("Only assistant messages can have tool_calls")
+    elif self.role == "tool" and self.tool_call_id is None:
+      raise ValueError("Tool messages must have a tool_call_id")
+
+    return self
+
+
+class ChatRequest(BaseModel):
+  messages: list[Message]
   stream: bool = False
+  temperature: float = 0.7
 
 weights_path = Path('weights/1B-Instruct')
 model_params = LLAMA_1B_PARAMS
@@ -63,24 +94,10 @@ async def health() -> Response:
   """Check the health of the http server."""
   return Response(status_code=200)
 
-@app.get("/health_generate")
-async def health_generate(request: Request) -> Response:
-  """Check the health of the inference server by generating one token."""
-  gri = GenerateReqInput(text="s", sampling_params={"max_new_tokens": 1, "temperature": 0.7})
-  try:
-    async for _ in tokenizer_manager.generate_request(gri, request):
-      break
-    return Response(status_code=200)
-  except Exception as e:
-    logger.exception(e)
-    return Response(status_code=503)
+@app.post("/v1/chat/completions")
+async def openai_chat_completions(request: ChatRequest):
+  logger.info(request.prompt)
 
-@app.post("/generate")
-def generate(request: PromptRequest):
-  # if request.stream:
-  # else:
-  #   return generate_all(request.prompt)
-  logging.info(request.prompt)
   tokens = tokenizer.encode(request.prompt, bos=False, eos=False, allowed_special='all')
 
   tokens = jnp.array([tokens], jnp.int32)
@@ -117,101 +134,16 @@ def generate_stream(tokens: Array, attn_mask: Array, freqs_cis: Array, kvcache: 
     if jnp.isin(next_token, stop).any():
       break
 
-def launch_engine(server_args: ServerArgs):
-  """
-    Launch the Tokenizer Manager in the main process, the Scheduler in a subprocess, and the Detokenizer Manager in another subprocess.
-    """
-
-  global tokenizer_manager
-
-  # Configure global environment
-  # configure_logger(server_args)
-  format = "[%(asctime)] %(message)s"
-  logging.basicConfig(
-      level=getattr(logging, server_args.log_level.upper()),
-      format=format,
-      datefmt="%H:%M:%S",
-      force=True,
-  )
-
-  # server_args.check_server_args()
-  # _set_envs_and_config(server_args)
-
-  # Allocate ports for inter-process communications
-  # port_args = PortArgs.init_new(server_args)
-  logger.info(f"{server_args=}")
-
-  # If using model from www.modelscope.cn, first download the model.
-  server_args.model_path, server_args.tokenizer_path = prepare_model_and_tokenizer(server_args.model_path, server_args.tokenizer_path)
-
-  # Launch tensor parallel scheduler processes
-  scheduler_procs = []
-  scheduler_pipe_readers = []
-  tp_size_per_node = server_args.tp_size // server_args.nnodes
-  tp_rank_range = range(
-      tp_size_per_node * server_args.node_rank,
-      tp_size_per_node * (server_args.node_rank + 1),
-  )
-  for tp_rank in tp_rank_range:
-    reader, writer = mp.Pipe(duplex=False)
-    gpu_id = tp_rank % tp_size_per_node
-    proc = mp.Process(
-        target=run_scheduler_process,
-        args=(server_args, port_args, gpu_id, tp_rank, writer),
-    )
-    proc.start()
-    scheduler_procs.append(proc)
-    scheduler_pipe_readers.append(reader)
-
-  if server_args.node_rank >= 1:
-    # For other nodes, they do not need to run tokenizer or detokenizer,
-    # so they can just wait here.
-    while True:
-      pass
-
-  # Launch detokenizer process
-  detoken_proc = mp.Process(
-      target=run_detokenizer_process,
-      args=(
-          server_args,
-          port_args,
-      ),
-  )
-  detoken_proc.start()
-
-  # Launch tokenizer process
-  tokenizer_manager = TokenizerManager(server_args, port_args)
-  if server_args.chat_template:
-    load_chat_template_for_openai_api(tokenizer_manager, server_args.chat_template)
-
-  # Wait for model to finish loading
-  for i in range(len(scheduler_pipe_readers)):
-    scheduler_pipe_readers[i].recv()
-
 def launch_server(server_args: ServerArgs):
-  """Launch server (based on SGLang Runtime Server)"""
-
-  launch_engine(server_args=server_args)
-
-  # Send a warmup request
-  t = threading.Thread(target=_wait_and_warmup, args=(server_args, pipe_finish_writer, os.getpid()))
-  t.start()
-
-  try:
-    # Listen for HTTP requests
-    uvicorn.run(
-      app,
-      host=server_args.host,
-      port=server_args.port,
-      log_level=server_args.log_level,
-      timeout_keep_alive=5,
-      loop="uvloop",
-    )
-  finally:
-    t.join()
+  uvicorn.run(
+    app,
+    host=server_args.host,
+    port=server_args.port,
+    log_level=server_args.log_level,
+    timeout_keep_alive=5,
+    loop="uvloop",
+  )
 
 if __name__ == "__main__":
-  # import uvicorn
-  # uvicorn.run(app, host="0.0.0.0", port=8000)
   server_args = tyro.cli(ServerArgs)
   launch_server(server_args)
